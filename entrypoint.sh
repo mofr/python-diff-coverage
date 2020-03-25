dbe9fc0662a745dc7c27001ea8e0aed0e1996f0a
Alexander Egorov <mofr@zond.org>
2020-03-25 18:06:05 +0700

    Add compare_ref input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-dir=$1
-report_dir=$2
-threshold=$3
+compare_ref=$1
+dir=$2
+report_dir=$3
+threshold=$4
 
 cd $dir
 coverage combine $report_dir
 coverage xml
-diff-cover coverage.xml --fail-under=threshold --compare-branch=origin/${{ github.base_ref }}
+diff-cover coverage.xml --fail-under=threshold --compare-branch=origin/$compare_ref
